e28cd3fa4344ada723acde7b5a6372bf0648139c
Wenliang Dai <wenliang.dai.17@ucl.ac.uk>
2018-06-11 17:03:14 +0100

    Transfer learning example

---
+++ utils/utils_fcn.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+
+def get_upsampling_weight(in_channels, out_channels, kernel_size):
+    """Make a 2D bilinear kernel suitable for upsampling"""
+    factor = (kernel_size + 1) // 2
+    if kernel_size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = np.ogrid[:kernel_size, :kernel_size]
+    filt = (1 - abs(og[0] - center) / factor) * (1 - abs(og[1] - center) / factor)
+    weight = np.zeros((in_channels, out_channels, kernel_size, kernel_size), dtype=np.float64)
+    weight[list(range(in_channels)), list(range(out_channels)), :, :] = filt
+    return torch.from_numpy(weight).float()
